dae3da6d2ea335f5809323c45e91ceb74f3bbda2
Jacob Tabor <jtabor214@gmail.com>
2023-11-13 16:50:51 -0500

    pushing

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -14,7 +14,6 @@ class MaxIntSet
     is_valid?(num)
     i = num % @store.length
     @store[i] = true
-    
   end
 
   def remove(num)
@@ -40,23 +39,33 @@ class MaxIntSet
 end
 
 class IntSet
+  attr_accessor :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return false if include?(num)
+      self[num] << num 
+      num
+  #   if include?(num) == false
+  #     self.store[num] << num
+  #   end
+  #   false
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store(num % num_buckets)
   end
 
   def num_buckets
